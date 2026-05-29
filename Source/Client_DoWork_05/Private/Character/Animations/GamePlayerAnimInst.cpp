576c030cc6f8c239f791d3191356c8efac3a3157
gimunkwon <gimon71@gmail.com>
2026-05-29 21:51:16 +0900

    [Add] AnimInstance Owner캐싱 Update메서드에서 Owner의 Speed값을 GroundSpeed 변수에 캐싱

--- Source/Client_DoWork_05/Private/Character/Animations/GamePlayerAnimInst.cpp
+++ Source/Client_DoWork_05/Private/Character/Animations/GamePlayerAnimInst.cpp
@@ -1,1 +1,22 @@
 #include "Character/Animations/GamePlayerAnimInst.h"
+
+#include "Character/GamePlayer.h"
+
+void UGamePlayerAnimInst::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	
+	MyOwner = Cast<AGamePlayer>(TryGetPawnOwner());
+}
+
+void UGamePlayerAnimInst::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	
+	if (!MyOwner)
+	{
+		return;
+	}
+	
+	GroundSpeed = MyOwner->GetVelocity().Size2D();
+}
--- Source/Client_DoWork_05/Public/Character/Animations/GamePlayerAnimInst.h
+++ Source/Client_DoWork_05/Public/Character/Animations/GamePlayerAnimInst.h
@@ -4,8 +4,22 @@
 #include "Animation/AnimInstance.h"
 #include "GamePlayerAnimInst.generated.h"
 
+class AGamePlayer;
+
 UCLASS()
 class CLIENT_DOWORK_05_API UGamePlayerAnimInst : public UAnimInstance
 {
 	GENERATED_BODY()
+	
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	
+protected:
+	UPROPERTY(VisibleAnywhere,BlueprintReadWrite,Category="Stat")
+	float GroundSpeed;
+	
+private:
+	UPROPERTY()
+	TObjectPtr<AGamePlayer> MyOwner;
 };

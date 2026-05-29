85c1b3b132ecda1156e12635efcae9a0288741c8
gimunkwon <gimon71@gmail.com>
2026-05-29 20:05:18 +0900

    [Add] AnimInstance 클래스 추가

---
+++ Source/Client_DoWork_05/Private/Character/Animations/GamePlayerAnimInst.cpp
@@ -0,0 +1,1 @@
+#include "Character/Animations/GamePlayerAnimInst.h"
---
+++ Source/Client_DoWork_05/Public/Character/Animations/GamePlayerAnimInst.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "GamePlayerAnimInst.generated.h"
+
+UCLASS()
+class CLIENT_DOWORK_05_API UGamePlayerAnimInst : public UAnimInstance
+{
+	GENERATED_BODY()
+};

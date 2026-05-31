f3b497dc165c9c9f38b767198dce170b21c25fc8
gimunkwon <gimon71@gmail.com>
2026-05-31 15:27:28 +0900

    [Add] ProgressBar 설정용 메서드 추가

--- Source/Client_DoWork_05/Private/UI/Widget_PlayerHUD.cpp
+++ Source/Client_DoWork_05/Private/UI/Widget_PlayerHUD.cpp
@@ -1,1 +1,12 @@
 #include "UI/Widget_PlayerHUD.h"
+
+#include "Components/ProgressBar.h"
+
+
+void UWidget_PlayerHUD::SetHPProgressBar(float MaxHP, float CurrentHP)
+{
+	if (PB_PlayerHP)
+	{
+		PB_PlayerHP->SetPercent(MaxHP / CurrentHP);
+	}
+}
--- Source/Client_DoWork_05/Public/UI/Widget_PlayerHUD.h
+++ Source/Client_DoWork_05/Public/UI/Widget_PlayerHUD.h
@@ -10,6 +10,8 @@ UCLASS()
 class CLIENT_DOWORK_05_API UWidget_PlayerHUD : public UUserWidget
 {
 	GENERATED_BODY()
+public:
+	void SetHPProgressBar(float MaxHP,float CurrentHP);
 	
 protected:
 	UPROPERTY(meta=(BindWidget))
